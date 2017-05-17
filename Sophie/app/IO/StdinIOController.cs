1baa2ec1e2a483fb0224570e470dd05d9a2a19fc
hasparus <hasparus@gmail.com>
2017-05-17 11:57:14 +0200

    Add .DotSettings

---
+++ Sophie/app/IO/StdinIOController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Sophie.Utils;
+
+namespace Sophie.IO
+{
+    public class StdinIOController : IOController
+    {
+        private const int ReadLineBufferSize = 1 << 13 /*8192*/; 
+        private void ChangeReadlineBufferSize()
+        {
+            
+        }
+
+        public StdinIOController()
+        {
+        }
+
+    }
+}
