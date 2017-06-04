d3c2f89ffad8f77ca19db7f1cbfa93c72016769d
hasparus <hasparus@gmail.com>
2017-06-04 07:38:35 +0200

    Add score.

--- Sophie/app/Logic.cs
+++ Sophie/app/Logic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Npgsql;
 using Sophie.DataLayer;
@@ -9,107 +10,107 @@ using Sophie.Utils;
 
 namespace Sophie
 {
-public static class Logic
-{
-    private static DataAccess _dataAccess;
-
-    public static JObject Call(string functionName, JObject parameters) =>
-        Functions[functionName]?.Invoke(parameters).ToJObject() ??
-        CallResult.NotImplemented.ToJObject();
-
-    private static Func<JObject, CallResult> SqlProcWrapper(
-        string procName, params string[] parameters)
+    public static class Logic
     {
-        return args =>
+        private static DataAccess _dataAccess;
+
+        public static JObject Call(string functionName, JObject parameters) =>
+            Functions[functionName]?.Invoke(parameters).ToJObject() ??
+            CallResult.NotImplemented.ToJObject();
+
+        private static Func<JObject, CallResult> SqlProcWrapper(
+            string procName, params string[] parameters)
         {
-            if (!ValidCallParameters(args, parameters))
-                return CallResult.Error(
-                    $"Invalid parameters for {procName} sql function.");
-
-            var actualParams = parameters.Select(
-                key => args[key].ToSqlString());
-
-            return _dataAccess.ExecuteSqlFromString(
-                $"select * from {procName}("
-                + string.Join(", ", actualParams)
-                + ");");
-        };
-    }
-
-    private static CallResult EstabilishConnection(JObject parameters)
-    {
-        var pms = AuthorizeConnect(parameters);
-        if (pms == null)
-            return CallResult.Error("Method EstabilishConnection got wrong parameters.");
-        (string dbName, string login, string password) = pms.Value;
-
-        _dataAccess = new DataAccess(
-            $"Host=localhost;Username={login};Password={password};Database={dbName}");
-        try
-        {
-            using (var connection = _dataAccess.NewConnection())
+            return args =>
             {
-                connection.Open();
+                if (!ValidCallParameters(args, parameters))
+                    return CallResult.Error(
+                        $"Invalid parameters for {procName} sql function.");
 
-                return connection.IsOpen()
-                    ? CallResult.Ok
-                    : CallResult.Error("Coudn't open connection to database.");
+                var actualParams = parameters.Select(
+                    key => args[key].ToSqlString());
+
+                return _dataAccess.ExecuteSqlFromString(
+                    $"select * from {procName}("
+                    + string.Join(", ", actualParams)
+                    + ");");
+            };
+        }
+
+        private static CallResult EstabilishConnection(JObject parameters)
+        {
+            var pms = AuthorizeConnect(parameters);
+            if (pms == null)
+                return CallResult.Error("Method EstabilishConnection got wrong parameters.");
+            (string dbName, string login, string password) = pms.Value;
+
+            _dataAccess = new DataAccess(
+                $"Host=localhost;Username={login};Password={password};Database={dbName}");
+            try
+            {
+                using (var connection = _dataAccess.NewConnection())
+                {
+                    connection.Open();
+
+                    return connection.IsOpen()
+                        ? CallResult.Ok
+                        : CallResult.Error("Coudn't open connection to database.");
+                }
+            }
+            catch (PostgresException e)
+            {
+                return CallResult.Error(e.Message);
+            }
+            catch (SocketException)
+            {
+                Debug.Log("Please enable socket access to PostgreSQL server.");
+                throw;
             }
         }
-        catch (PostgresException e)
+
+        private static CallResult Setup(JObject parameters)
         {
-            return CallResult.Error(e.Message);
+            if (AuthorizeConnect(parameters) == null)
+                return CallResult.Error("Method Setup got wrong parameters.");
+
+            try
+            {
+                using (var connection = _dataAccess.NewConnection())
+                    return _dataAccess.ExecuteSqlFromFile("db/open.sql", connection);
+            }
+            catch (PostgresException e)
+            {
+                return CallResult.Error(e.Message);
+            }
+            catch (SocketException)
+            {
+                Debug.Log("Please enable socket access to PostgreSQL server.");
+                throw;
+            }
         }
-        catch (SocketException)
+
+        private static CallResult Open(JObject parameters)
         {
-            Debug.Log("Please enable socket access to PostgreSQL server.");
-            throw;
+            var est = EstabilishConnection(parameters);
+            return est == CallResult.Ok ? Setup(parameters) : est;
         }
-    }
 
-    private static CallResult Setup(JObject parameters)
-    {
-        if (AuthorizeConnect(parameters) == null)
-            return CallResult.Error("Method Setup got wrong parameters.");
-
-        try
+        private static (string, string, string)? AuthorizeConnect(JObject parameters)
         {
-            using (var connection = _dataAccess.NewConnection())
-                return _dataAccess.ExecuteSqlFromFile("db/open.sql", connection);
+            if (!ValidCallParameters(parameters, new[] { "baza", "login", "password" }))
+                return null;
+
+            return (parameters["baza"].ToSqlString(),
+                    parameters["login"].ToSqlString(),
+                    parameters["password"].ToSqlString());
         }
-        catch (PostgresException e)
-        {
-            return CallResult.Error(e.Message);
-        }
-        catch (SocketException)
-        {
-            Debug.Log("Please enable socket access to PostgreSQL server.");
-            throw;
-        }
-    }
 
-    private static CallResult Open(JObject parameters)
-    {
-        var est = EstabilishConnection(parameters);
-        return est == CallResult.Ok ? Setup(parameters) : est;
-    }
+        private static bool ValidCallParameters(JObject given, IEnumerable<string> requested)
+            => requested.All(p => given[p] != null);
 
-    private static (string, string, string)? AuthorizeConnect(JObject parameters)
-    {
-        if (!ValidCallParameters(parameters, new[] { "baza", "login", "password" }))
-            return null;
-
-        return (parameters["baza"].ToSqlString(),
-                parameters["login"].ToSqlString(),
-                parameters["password"].ToSqlString());
-    }
-
-    private static bool ValidCallParameters(JObject given, IEnumerable<string> requested)
-        => requested.All(p => given[p] != null);
-
-    private static readonly ReferenceHash<string, Func<JObject, CallResult>> Functions =
-        new ReferenceHash<string, Func<JObject, CallResult>>
-        {
+        private static readonly ReferenceHash<string, Func<JObject, CallResult>> Functions =
+            new ReferenceHash<string, Func<JObject, CallResult>>
+            {
             {"open", Open},
             {"connect", EstabilishConnection},
             {"setup", Setup},
@@ -118,8 +119,8 @@ public static class Logic
                     if (_dataAccess?.NewConnection() == null)
                         return CallResult.Error("Can't drop if connection isn't estabilished");
                     if (!ValidCallParameters(args, new[] {"secret"})
-                        || args["secret"].ToSqlString() != "42")
-                        return CallResult.Error("Haha. No dropping if you don't know secret.");
+                        || args["secret"].ToString() != "42")
+                        return CallResult.Error($"{args["secret"].ToSqlString()} is not the secret.");
                     return _dataAccess.ExecuteSqlFromFile("db/drop.sql");
                 }
             },
@@ -194,11 +195,12 @@ public static class Logic
                         return CallResult.Error("Wrong arguments for recommended talks.");
 
                     var callResult = _dataAccess.ExecuteSqlFromString(
-                        $"select * from participant where login = '{args["login"].ToSqlString()}' and password_hash = '{args["password"].ToSqlString()}'");
+                        $"select * from participant where login = {args["login"].ToSqlString()} and password_hash = {args["password"].ToSqlString()}");
 
                     Debug.Log("request author: ");
                     Debug.Log(callResult.ToString());
 
+                    CallResult result = new CallResult(CallResult.Status.Error);
                     if (callResult.Data != null && callResult.Data.Count > 0)
                     {
                         var user = callResult.Data[0];
@@ -211,38 +213,124 @@ public static class Logic
                         }
 
                         var limit = args["limit"].Value<long>() == 0
-                            ? 9223372036854775807
+                            ? long.MaxValue
                             : args["limit"].Value<long>();
 
+                        var st = args["start_timestamp"].ToSqlString();
+                        var et = args["end_timestamp"].ToSqlString();
+
                         var ratings = _dataAccess.ExecuteSqlFromString(
-                            $@"select t.id, sum(r.rating) ratingSum
+                            $@"select t.id talk, sum(r.rating) sum, count(r.rating) count
                                 from talk t
                                 join rating r on (t.id = r.talk_id)
-                                where t.start_timestamp >= '2016-01-01'
-                                    and t.start_timestamp <= '2018-01-01'
+                                where t.start_timestamp >= {st}
+                                    and t.start_timestamp <= {et}
                                     and t.rejected_timestamp is null
                                     and t.accepted_timestamp is not null
                                 group by t.id;");
 
                         var attendances = _dataAccess.ExecuteSqlFromString(
-                            $@"select t.id, count(*) attendances
+                            $@"select t.id talk, count(*) attendances
                                 from talk t
                                 join attendance a on (t.id = a.talk_id)
-                                where t.start_timestamp >= '2016-01-01'
-                                    and t.start_timestamp <= '2018-01-01'
+                                where t.start_timestamp >= {st}
+                                    and t.start_timestamp <= {et}
                                     and t.rejected_timestamp is null
                                     and t.accepted_timestamp is not null
                                 group by t.id;");
 
+                        var attendeeRatings = _dataAccess.ExecuteSqlFromString(
+                            $@"select t.id talk, sum(r.rating) sum, count(r.rating) count
+                                from talk t
+                                join rating r on (t.id = r.talk_id)
+                                join attendance using (talk_id, participant_id) 
+                                where t.start_timestamp >= {st}
+                                  and t.start_timestamp <= {et}
+                                  and t.rejected_timestamp is null
+                                  and t.accepted_timestamp is not null
+                                group by t.id");
+
+                        const int friendshipBonus = 20;
+                        var friendBonuses = _dataAccess.ExecuteSqlFromString(
+                            $@"select t.id talk, {friendshipBonus} bonus
+                                from talk t
+                                join friendship_declaration fd on (t.speaker_id = fd.receiver_id)
+                                where t.start_timestamp >= {st}
+                                  and t.start_timestamp <= {et}
+                                  and t.rejected_timestamp is null
+                                  and t.accepted_timestamp is not null
+                                  and fd.declarer_id = {user["id"]}");
+
                         Debug.Log("---");
                         Debug.Log(ratings.ToString());
                         Debug.Log(attendances.ToString());
-                        Debug.Log("---");
-                    }
+                        Debug.Log(attendeeRatings.ToString());
+                        Debug.Log(friendBonuses.ToString());
 
-                    return CallResult.NotImplemented;
+                        var rat = Dearrayify(ratings.Data);
+                        var att = Dearrayify(attendances.Data);
+                        var atr = Dearrayify(attendeeRatings.Data);
+                        var frb = Dearrayify(friendBonuses.Data);
+
+                        result = _dataAccess.ExecuteSqlFromString(
+                            $@"select t.id talk, speaker.login speakerlogin, t.start_timestamp, t.title, t.room
+                                from talk t
+                                join participant speaker on (t.speaker_id = speaker.id)
+                                where t.start_timestamp >= {st}
+                                  and t.start_timestamp <= {et}
+                                  and t.rejected_timestamp is null
+                                  and t.accepted_timestamp is not null
+                                limit {limit};");
+
+                        Debug.Log(result.ToString());
+
+                        if (result.State == CallResult.Status.Error)
+                            return CallResult.Error("Score failed.");
+
+                        foreach (var jToken in result.Data)
+                        {
+                            var jo = (JObject) jToken;
+                            jo.Add("score", Score(jo["talk"].Value<string>(), att, rat, atr, frb));
+                        }
+                        Debug.Log("---");
+
+                    }
+                    return result;
                 }
             }
-        };
-}
+            };
+
+        private static int Score
+            (string talk, JObject attendances, JObject ratings, JObject attendeeRatings, JObject friendBonuses)
+        {
+            try
+            {
+                const int middleScore = 5;
+                var att = attendances[talk] != null ? attendances[talk]["attendances"].Value<int>() : 0;
+                var ratingSum = ratings[talk] != null ? ratings[talk]["sum"].Value<int>() : 0;
+                var ratingCount = ratings[talk] != null ? ratings[talk]["count"].Value<int>() : 0;
+                var aRatingSum = ratings[talk] != null ? attendeeRatings[talk]["sum"].Value<int>() : 0;
+                var aRatingCount = ratings[talk] != null ? attendeeRatings[talk]["count"].Value<int>() : 0;
+                var fBonus = friendBonuses[talk] != null ? friendBonuses[talk]["bonus"].Value<int>() : 0;
+                return att + ratingSum - middleScore * ratingCount + aRatingSum - middleScore * aRatingCount + fBonus;
+            }
+            catch (JsonException) //bad design
+            {
+                return 0;
+            }
+        }   
+
+        private static JObject Dearrayify(JArray jarr)
+        {
+            JObject res = new JObject();
+            if (jarr != null)
+                foreach (var jToken in jarr)
+                {
+                    var jo = jToken as JObject;
+                    if (jo == null) continue;
+                    res.Add(jo["talk"].Value<string>(), jo);
+                }
+            return res;
+        }
+    }
 }
